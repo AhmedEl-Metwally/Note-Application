84f2d8c25b8657319955469add18ede140bcb0ec
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-03-23 22:58:04 +0200

    notes services

--- src/app/core/services/notes/notes.service.ts
+++ src/app/core/services/notes/notes.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
-export class NotesService {
+export class NotesService
+{
+
+  private readonly _httpClient = inject(HttpClient)
+
 
   constructor() { }
+
+
+  addNote(data:object): Observable<any>
+  {
+    return this._httpClient.post(`${environment.noteUrl}`,data)
+  }
+
+  getUserNotes(): Observable<any>
+  {
+    return this._httpClient.get(`${environment.noteUrl}`)
+  }
+
+  updateNote(id: string, data: object): Observable<any>
+  {
+    return this._httpClient.put(`${environment.noteUrl}${id}`,data)
+  }
+
+  deleteNote(id: string): Observable<any>
+  {
+    return this._httpClient.delete(`${environment.noteUrl}${id}`)
+  }
+
+
+
+
+
+
+
 }
